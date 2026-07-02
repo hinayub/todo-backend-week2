8167a005652587bf21ba2bbf6dff5ab1db8d92f1
Hina Ayub <hinaayub2002@gmail.com>
2026-07-03 00:17:31 +0500

    ruff installed

--- config/urls.py
+++ config/urls.py
@@ -14,14 +14,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path
-from django.urls import path,include
 
+from django.contrib import admin
+from django.urls import include, path
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/auth/',include('apps.authentication.urls')),
-    path('api/tasks/', include('apps.tasks.urls')),
-
+    path("admin/", admin.site.urls),
+    path("api/auth/", include("apps.authentication.urls")),
+    path("api/tasks/", include("apps.tasks.urls")),
 ]
